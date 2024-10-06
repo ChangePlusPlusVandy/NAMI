78364f0e26332ae94837d361d995743d26c8cda8
zacharytao <zacharywtao@gmail.com>
2024-10-05 20:16:18 -0500

    first commit

---
+++ NAMI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NAMI
+//
+//  Created by Zachary Tao on 10/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello world")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NAMI/NAMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAMIApp.swift
+//  NAMI
+//
+//  Created by Zachary Tao on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NAMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
