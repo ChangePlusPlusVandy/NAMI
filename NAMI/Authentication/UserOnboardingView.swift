6a8a2322b381faa668ae301e187867529d8e8778
zacharytao <zacharywtao@gmail.com>
2024-11-12 17:47:52 -0600

    refactor onboarding

--- NAMI/Authentication/UserOnboardingView.swift
+++ NAMI/Authentication/UserOnboardingView.swift
@@ -10,147 +10,76 @@ import SwiftUI
 
 struct UserOnboardingView: View {
     @State var user = NamiUser(userType: .member, firstName: "", lastName: "", email: "", phoneNumber: "", zipCode: "")
-//    @Binding var user: NamiUser
     @Environment(AuthenticationManager.self) var authManager
     @Environment(\.dismiss) var dismiss
-    
+
     var body: some View {
-        VStack (alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) {
-            Spacer()
-                .frame(height: 50)
+        VStack(alignment: .leading, spacing: 16) {
+
             Text("Member Sign Up")
                 .font(.title)
-            Spacer()
-                .frame(height: 35)
-            HStack {
-                Spacer()
-                    .frame(width: 15)
-                Text("First Name: ")
-                    .bold()
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                TextField("First Name", text: $user.firstName)
-                    .frame(width: 365, height: 50)
-                    .padding(5)
-                    .background {
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 5)
-                            .frame(width: 375, height: 65)
-                            .foregroundStyle(.gray)
-                            .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-                    }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 10)
-            HStack {
-                Spacer()
-                    .frame(width: 15)
-                Text("Last Name: ")
-                    .bold()
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                TextField("Last Name", text: $user.lastName)
-                    .frame(width: 365, height: 50)
-                    .padding(5)
-                    .background {
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 5)
-                            .frame(width: 375, height: 65)
-                            .foregroundStyle(.gray)
-                            .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-                    }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 10)
-            HStack {
-                Spacer()
-                    .frame(width: 15)
-                Text("Email: ")
-                    .bold()
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                TextField("Email", text: $user.email)
-                    .frame(width: 365, height: 50)
-                    .padding(5)
-                    .background {
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 5)
-                            .frame(width: 375, height: 65)
-                            .foregroundStyle(.gray)
-                            .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-                    }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 10)
-            HStack {
-                Spacer()
-                    .frame(width: 15)
-                Text("Phone number: ")
-                    .bold()
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                TextField("Phone Number", text: $user.phoneNumber)
-                    .frame(width: 365, height: 50)
-                    .padding(5)
-                    .background {
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 5)
-                            .frame(width: 375, height: 65)
-                            .foregroundStyle(.gray)
-                            .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-                    }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 10)
-            HStack {
-                Spacer()
-                    .frame(width: 15)
-                Text("Zipcode: ")
-                    .bold()
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                TextField("Zipcode", text: $user.zipCode)
-                    .frame(width: 365, height: 50)
-                    .padding(5)
-                    .background {
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 5)
-                            .frame(width: 375, height: 65)
-                            .foregroundStyle(.gray)
-                            .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-                    }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 40)
-            HStack {
-                Spacer()
-                Button {
-                    Task {
-                        await UserManager.shared.createNewUser(newUser: NamiUser.errorUser)
-                    }
-                    authManager.isFirstTimeSignIn = false
-                } label: {
-                    Text("Submit")
-                        .font(.title2)
+                .bold()
+
+            Text("First Name: ")
+                .bold()
+
+            TextField("First Name", text: $user.firstName)
+                .textFieldStyle(CustemOnboardingTextfieldStyle())
+
+
+            Text("Last Name: ")
+                .bold()
+
+
+            TextField("Last Name", text: $user.lastName)
+                .textFieldStyle(CustemOnboardingTextfieldStyle())
+
+            Text("Email: ")
+                .bold()
+
+
+            TextField("Email", text: $user.email)
+                .textFieldStyle(CustemOnboardingTextfieldStyle())
+
+
+            Text("Phone number: ")
+                .bold()
+
+
+            TextField("Phone Number", text: $user.phoneNumber)
+                .textFieldStyle(CustemOnboardingTextfieldStyle())
+
+            Text("Zipcode: ")
+                .bold()
+
+
+            TextField("Zipcode", text: $user.zipCode)
+                .textFieldStyle(CustemOnboardingTextfieldStyle())
+
+
+            Button {
+                Task {
+                    await UserManager.shared.createNewUser(newUser: NamiUser.errorUser)
                 }
-                Spacer()
+                authManager.isFirstTimeSignIn = false
+            } label: {
+                Text("Confirm")
+                    .font(.title2)
             }
-        }
+            .frame(maxWidth: .infinity, alignment: .center)
+
+        }.padding()
+    }
+}
+
+struct CustemOnboardingTextfieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.gray, lineWidth: 2)
+            )
     }
 }
 
