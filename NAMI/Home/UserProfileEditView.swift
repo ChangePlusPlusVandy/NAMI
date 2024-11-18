3268c032283edc083b499b5570908eaf39e29bf9
Riley Koo <rkoo@berkeley.edu>
2024-11-18 11:27:41 -0600

    Created ProfileEditView

--- NAMI/Home/UserProfileEditView.swift
+++ NAMI/Home/UserProfileEditView.swift
@@ -9,8 +9,53 @@ import SwiftUI
 
 struct UserProfileEditView: View {
     @Environment(HomeScreenRouter.self) var homeScreenRouter
+    @State var user: NamiUser = (UserManager.shared.getCurrentUser() ?? NamiUser(userType: .member, firstName: "", lastName: "", email: "", phoneNumber: "", zipCode: ""))
     var body: some View {
-        Text("This view is the user profile edit view!")
+        HStack {
+            Button {
+                homeScreenRouter.navigateBack()
+            } label: {
+                HStack {
+                    Image(systemName: "chevron.backward")
+                        .padding(2)
+                    Text("Cancel")
+                        .padding(2)
+                }
+            }
+            .padding(10)
+            .frame(alignment: .leading)
+            Spacer()
+            Button {
+                Task {
+                    await UserManager.shared.updateUserInfo(updatedUser: user)
+                }
+                homeScreenRouter.navigateBack()
+            } label: {
+                Text("Save")
+            }
+            .padding(10)
+            .frame(alignment: .trailing)
+        }
+        ScrollView (showsIndicators: false) {
+            VStack(alignment: .leading, spacing: 21) {
+
+                Text("Profile")
+                    .font(.title.bold())
+                    .padding(20)
+
+                UserInfoInputField(text: "First Name", field: $user.firstName)
+
+                UserInfoInputField(text: "Last Name", field: $user.lastName)
+
+                UserInfoInputField(text: "Email", field: $user.email)
+
+                UserInfoInputField(text: "Phone Number", field: $user.phoneNumber, numPad: true)
+
+                UserInfoInputField(text: "ZIP Code", field: $user.zipCode, numPad: true)
+
+            }
+        }.scrollDismissesKeyboard(.interactively)
+
     }
 }
 
