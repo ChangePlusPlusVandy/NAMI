efb4c5147a8bb78f8bb47c8ef68a912a9f3bd7ee
zacharytao <zacharywtao@gmail.com>
2024-11-24 12:17:03 -0600

    tab view refactor

--- NAMI/AppView.swift
+++ NAMI/AppView.swift
@@ -8,32 +8,39 @@
 import SwiftUI
 
 struct AppView: View {
-
+    
+    @State var tabSelection = 0
+    
+    
     init() {
         // to customize tab bar color
         let tabBarAppearance = UITabBarAppearance()
         tabBarAppearance.stackedLayoutAppearance.selected.iconColor = UIColor(Color.NAMIDarkBlue)
         tabBarAppearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor(Color.NAMIDarkBlue)]
-
+        
         UITabBar.appearance().standardAppearance = tabBarAppearance
         UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
     }
-
+    
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
-            EventsView()
-                .tabItem {
-                    Label("Events", systemImage: "person.3.fill")
-                }
-            ChatView()
-                .tabItem {
-                    Label("Chat", systemImage: "message")
-                }
+        TabView(selection: $tabSelection) {
+            Tab(value: 0) {
+                HomeView()
+            } label: {
+                Label("Home", systemImage: "house")
+            }
+            Tab(value: 1) {
+                EventsView()
+            } label: {
+                Label("Events", systemImage: "person.3.fill")
+            }
+            Tab(value: 2) {
+                ChatView()
+            } label: {
+                Label("Chat", systemImage: "message")
+            }
         }
+        .sensoryFeedback(.impact(weight: .heavy), trigger: tabSelection)
     }
 }
 
