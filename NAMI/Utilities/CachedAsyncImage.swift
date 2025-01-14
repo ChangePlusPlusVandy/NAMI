1507e5b543ded2e5359133577fabd08c4b3784ac
zacharytao <zacharywtao@gmail.com>
2025-01-14 11:12:03 -0600

    fix cache image bug

--- NAMI/Utilities/CachedAsyncImage.swift
+++ NAMI/Utilities/CachedAsyncImage.swift
@@ -9,56 +9,42 @@ import SwiftUI
 
 struct CachedAsyncImage: View {
     let url: String
-    
+
     var body: some View {
-      if let cachedImage = loadCachedImage() {
-        cachedImage
-          .resizable()
-          .scaledToFit()
-      } else {
-        AsyncImage(url: URL(string: url)) { image in
-            image
+        if let cachedImage = loadCachedImage() {
+            cachedImage
                 .resizable()
                 .scaledToFit()
-                .task {
-                    cacheImage(image)
-                }
-        } placeholder: {
-
+        } else {
+            AsyncImage(url: URL(string: url)) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .task {
+                        await cacheImage()
+                    }
+            } placeholder: { }
         }
-      }
     }
-    
-    private func cacheImage(_ image: Image) {
-        guard let uiImage = image.asUIImage(),
-              let data = uiImage.jpegData(compressionQuality: 1.0) else { return }
-        
-        try? CacheManager.shared.insert(
-            id: url,
-            data: data,
-            expirationDays: 7
-        )
+
+    private func cacheImage() async {
+        do {
+            guard let tempURL = URL(string: url) else { return }
+            let (data, _) = try await URLSession.shared.data(from: tempURL)
+
+            try? CacheManager.shared.insert(
+                id: url,
+                data: data,
+                expirationDays: 3
+            )
+        } catch {
+            print("unable to cache image \(error.localizedDescription)")
+        }
     }
-    
+
     private func loadCachedImage() -> Image? {
         guard let cachedData = try? CacheManager.shared.get(id: url)?.data,
               let uiImage = UIImage(data: cachedData) else { return nil }
         return Image(uiImage: uiImage)
-    }
-}
-
-// Helper extension
-private extension Image {
-    func asUIImage() -> UIImage? {
-        let controller = UIHostingController(rootView: self)
-        let view = controller.view
-        let targetSize = controller.view.intrinsicContentSize
-        view?.bounds = CGRect(origin: .zero, size: targetSize)
-        view?.backgroundColor = .clear
-        
-        let renderer = UIGraphicsImageRenderer(size: targetSize)
-        return renderer.image { _ in
-            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
-        }
     }
 }
